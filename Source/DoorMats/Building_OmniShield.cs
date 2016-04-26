d58f6f9d3724eb78832ec77640645064f9cdcd17
Skullywag <mjskull@gmail.com>
2016-04-26 22:12:24 +0100

    alpha 13 release on behalf of Latta

---
+++ Source/DoorMats/Building_OmniShield.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+using Verse;
+namespace DoorMats
+{
+    public class Building_DoorMat : Building
+    {
+        public override void Tick()
+        {
+            if (!Gen.IsHashIntervalTick(this, 10))
+            {
+                return;
+            }
+            base.Tick();
+            foreach (IntVec3 current in GenAdj.OccupiedRect(this))
+            {
+                if (!GenGrid.Impassable(current))
+                {
+                    Pawn pawn = Find.Map.thingGrid.ThingAt<Pawn>(current);
+                    if (pawn != null)
+                    {
+                        pawn.filth.GetType().GetMethod("TryDropFilth", BindingFlags.Instance | BindingFlags.NonPublic).Invoke(pawn.filth, null);
+                    }
+                }
+            }
+        }
+    }
+}
